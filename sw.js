3615784f298053a762dc436c4568ee22e764df8a
EuheniHreben <tomasblack3@gmail.com>
2026-09-05 21:12:56 +0300

    add manifest

--- sw.js
+++ sw.js
@@ -1,3 +1,28 @@
-self.addEventListener('fetch', (event) => {
-  // Этот пустой обработчик нужен, чтобы браузер признал сайт полноценным PWA
+const CACHE_NAME = "todo-focus-v1";
+// Указываем файлы, которые нужно сохранить в памяти телефона
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png",
+];
+
+// Установка: сохраняем файлы в кэш
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }),
+  );
+});
+
+// Перехват запросов: отдаем файлы из кэша, если нет сети
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    }),
+  );
 });
